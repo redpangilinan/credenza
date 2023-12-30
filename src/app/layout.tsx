de125c0d91787956225e1272e8d617c92189ef31
redpangilinan <redpangilinan715@gmail.com>
2023-12-30 19:26:40 +0800

    refactor: change default theme to white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="white"
           enableSystem
           disableTransitionOnChange
         >
