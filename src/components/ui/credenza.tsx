861b9c432087f82bcb225468ce4e3da1e80cf5f0
redpangilinan <redpangilinan715@gmail.com>
2024-01-11 20:18:17 +0800

    refactor: update formatting

--- src/components/ui/credenza.tsx
+++ src/components/ui/credenza.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import * as React from "react"
-import { useMediaQuery } from "@/hooks/use-media-query"
 
 import { cn } from "@/lib/utils"
+import { useMediaQuery } from "@/hooks/use-media-query"
 import {
   Dialog,
   DialogClose,
