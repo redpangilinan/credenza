db5615a3889764ceb47f5bf1a8523326f1724817
redpangilinan <redpangilinan715@gmail.com>
2024-05-03 18:38:07 +0800

    refactor: move credenza modal to a separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,67 +30,7 @@ export default function Home() {
         </p>
         <div className="flex gap-2">
           <ModeToggle />
-          <Credenza>
-            <CredenzaTrigger asChild>
-              <Button>Open modal</Button>
-            </CredenzaTrigger>
-            <CredenzaContent>
-              <CredenzaHeader>
-                <CredenzaTitle>Credenza</CredenzaTitle>
-                <CredenzaDescription>
-                  A responsive modal component for shadcn/ui.
-                </CredenzaDescription>
-              </CredenzaHeader>
-              <CredenzaBody className="space-y-4 pb-4 text-center text-sm sm:pb-0 sm:text-left">
-                <p>
-                  This component is built using shadcn/ui&apos;s dialog and
-                  drawer component, which is built on top of Vaul.
-                </p>
-                <p>
-                  It shows a{" "}
-                  <Link
-                    href="https://ui.shadcn.com/docs/components/dialog"
-                    target="_blank"
-                    className="underline"
-                  >
-                    dialog modal
-                  </Link>{" "}
-                  for desktop view and a{" "}
-                  <Link
-                    href="https://vaul.emilkowal.ski/"
-                    target="_blank"
-                    className="underline"
-                  >
-                    bottom drawer
-                  </Link>{" "}
-                  for mobile view.
-                </p>
-                <p>
-                  The documentation for installation and usage can be found{" "}
-                  <Link
-                    href={`${siteConfig.links.github}/blob/main/README.md`}
-                    target="_blank"
-                    className="underline"
-                  >
-                    here
-                  </Link>
-                  .
-                </p>
-              </CredenzaBody>
-              <CredenzaFooter>
-                <Link
-                  href={`${siteConfig.links.github}/blob/main/README.md`}
-                  target="_blank"
-                  className={cn(buttonVariants({ variant: "default" }))}
-                >
-                  Documentation
-                </Link>
-                <CredenzaClose asChild>
-                  <Button variant="outline">Close</Button>
-                </CredenzaClose>
-              </CredenzaFooter>
-            </CredenzaContent>
-          </Credenza>
+          <OpenModal />
           <Link
             href={siteConfig.links.github}
             target="_blank"
@@ -102,5 +42,71 @@ export default function Home() {
         </div>
       </div>
     </main>
+  )
+}
+
+function OpenModal() {
+  return (
+    <Credenza>
+      <CredenzaTrigger asChild>
+        <Button>Open modal</Button>
+      </CredenzaTrigger>
+      <CredenzaContent>
+        <CredenzaHeader>
+          <CredenzaTitle>Credenza</CredenzaTitle>
+          <CredenzaDescription>
+            A responsive modal component for shadcn/ui.
+          </CredenzaDescription>
+        </CredenzaHeader>
+        <CredenzaBody className="space-y-4 pb-4 text-center text-sm sm:pb-0 sm:text-left">
+          <p>
+            This component is built using shadcn/ui&apos;s dialog and drawer
+            component, which is built on top of Vaul.
+          </p>
+          <p>
+            It shows a{" "}
+            <Link
+              href="https://ui.shadcn.com/docs/components/dialog"
+              target="_blank"
+              className="underline"
+            >
+              dialog modal
+            </Link>{" "}
+            for desktop view and a{" "}
+            <Link
+              href="https://vaul.emilkowal.ski/"
+              target="_blank"
+              className="underline"
+            >
+              bottom drawer
+            </Link>{" "}
+            for mobile view.
+          </p>
+          <p>
+            The documentation for installation and usage can be found{" "}
+            <Link
+              href={`${siteConfig.links.github}/blob/main/README.md`}
+              target="_blank"
+              className="underline"
+            >
+              here
+            </Link>
+            .
+          </p>
+        </CredenzaBody>
+        <CredenzaFooter>
+          <Link
+            href={`${siteConfig.links.github}/blob/main/README.md`}
+            target="_blank"
+            className={cn(buttonVariants({ variant: "default" }))}
+          >
+            Documentation
+          </Link>
+          <CredenzaClose asChild>
+            <Button variant="outline">Close</Button>
+          </CredenzaClose>
+        </CredenzaFooter>
+      </CredenzaContent>
+    </Credenza>
   )
 }
